d50c9e2a9bc3ea5102857e546b89a0b4d0c68bd8
Your Name <you@example.com>
2021-09-16 13:46:19 +0300

    create_main_windows

---
+++ DMS MySql/DMS MySql/FolderConnect.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DMS_MySql
+{
+    /// <summary>
+    /// Логика взаимодействия для FolderConnect.xaml
+    /// </summary>
+    public partial class FolderConnect : Window
+    {
+        public FolderConnect()
+        {
+            InitializeComponent();
+        }
+        public void Connect_FolderDB(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- DMS MySql/DMS MySql/NetworkConnect.xaml.cs
+++ DMS MySql/DMS MySql/NetworkConnect.xaml.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Xml;
-using System.Xml.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,19 +8,23 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace DMS_MySql
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для NetworkConnect.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NetworkConnect : Window
     {
-        public MainWindow()
+
+        public NetworkConnect()
         {
             InitializeComponent();
+        }
+        public void Connect_DB(object sender, RoutedEventArgs e)
+        {
+            
         }
         public void Exit_button(object sender, RoutedEventArgs e)
         {
@@ -44,13 +44,11 @@ namespace DMS_MySql
         }
         public void Button_Main_Network(object sender, RoutedEventArgs e)
         {
-            new NetworkConnect().Show();
-            this.Close();
+
         }
         public void Button_Settings_Open(object sender, RoutedEventArgs e)
         {
 
         }
     }
-   
 }
